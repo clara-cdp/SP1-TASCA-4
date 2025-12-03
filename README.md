525bb586e5640c3b34a5a641fc6e12a7e4fb3dcd
clara-cdp <clarianne.cdp@gmail.com>
2025-12-03 13:34:54 +0100

    nivell-1 completed

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SP1-TASCA-4
 PHP - POO - 1 
+
+_Creació i ús de classes i objectes a PHP._
+_Ús d’herència i composició com a mecanismes de relació entre classes/objectes._
---
+++ T4-nivell1-ex2.php
@@ -0,0 +1,41 @@
+<?php
+/*Escriu un programa que defineixi una classe Shape amb un constructor que rebi com a paràmetres l'ample i alt. 
+Defineix dues subclasses; Triangle i Rectangle que heretin de Shape i que calculin respectivament l'àrea amb un mètode.*/
+
+class shape
+{
+    public float $width;
+    public float $height;
+
+
+    function __construct(float $width, float $height)
+    {
+        $this->width = $width;
+        $this->height = $height;
+    }
+
+    function calcArea() {}
+}
+
+class triangle extends shape
+{
+
+    function calcArea()
+    {
+        return (float) $area = ($this->width * $this->height) / 2;
+    }
+}
+
+class rectangle extends shape
+{
+    function calcArea()
+    {
+        return (float) $area = ($this->width * $this->height);
+    }
+}
+
+$shape1 = new triangle(5.2, 6);
+echo "The area is: " . $shape1->calcArea() . PHP_EOL;
+
+$shape2 = new rectangle(3, 6.8);
+echo "The area is: " . $shape2->calcArea() . PHP_EOL;
